269e2378fa0d1fd50d14b42b494af39fbee04d53
Rebeka Meszaros <akeber.m@live.com>
2020-09-11 15:16:45 +0100

    Updates

---
+++ precache-manifest.879577ee3e49c3c16e929fed4e554fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab5dcd4dcf70ed6e22fd128d7daf79c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "156c3296503940fea71e",
+    "url": "/static/css/main.557b5185.chunk.css"
+  },
+  {
+    "revision": "fcd4a560e38c9149d8b5",
+    "url": "/static/js/2.e6907142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6907142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156c3296503940fea71e",
+    "url": "/static/js/main.f3181fb0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
